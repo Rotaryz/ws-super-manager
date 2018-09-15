8dbece2bb7a2f3b2d2aa3ec871ebbafc05f0a0d4
Rotary <zhouxuan@jkweixin.com>
2018-09-15 15:27:52 +0800

    请求域名

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -9,11 +9,11 @@ const DEV_URL = {
 }
 
 const TEST_URL = {
-  api: 'https://ws-super-manager.jkweixin.net'
+  api: 'https://ws-api.jkweixin.net'
 }
 
 const PROD_URL = {
-  api: 'https://ws-super-manager.jkweixin.com'
+  api: 'https://ws-api.jkweixin.com'
 }
 
 export const BASE_URL = env.NODE_ENV === 'production' ? PROD_URL : env.NODE_ENV === 'test' ? TEST_URL : env.NODE_ENV === 'dev' ? DEV_URL : LOCAL_URL
