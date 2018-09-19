c02003c7e0697aea481b540da7228cb0760ad6f3
Rotary <zhouxuan@jkweixin.com>
2018-09-19 16:10:41 +0800

    修改尺寸

--- src/components/base-model/base-model.vue
+++ src/components/base-model/base-model.vue
@@ -134,7 +134,7 @@
   @import "~common/stylus/variable"
   @import '~common/stylus/mixin'
   .base-model
-    min-width: 1295px
+    min-width: 1410px
     position: relative
     display: flex
     overflow: auto
